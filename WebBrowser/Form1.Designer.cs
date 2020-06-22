ad17646d22ab93ad92d96324f6e3e9a3c7f79689
Nepcam <cameron.nepe@gmail.com>
2020-06-23 10:40:13 +1200

    UI drafting

--- WebBrowser/Form1.Designer.cs
+++ WebBrowser/Form1.Designer.cs
@@ -28,13 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.webBrowserApp = new System.Windows.Forms.WebBrowser();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // webBrowserApp
+            // 
+            this.webBrowserApp.Location = new System.Drawing.Point(0, 0);
+            this.webBrowserApp.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserApp.Name = "webBrowserApp";
+            this.webBrowserApp.Size = new System.Drawing.Size(800, 450);
+            this.webBrowserApp.TabIndex = 0;
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(183, 15);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(482, 20);
+            this.textBoxURL.TabIndex = 1;
+            this.textBoxURL.Text = "Enter URL";
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(12, 418);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.Size = new System.Drawing.Size(171, 20);
+            this.textBoxStatus.TabIndex = 2;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(8, 14);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 3;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.Location = new System.Drawing.Point(89, 14);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(75, 23);
+            this.buttonForward.TabIndex = 4;
+            this.buttonForward.Text = "Forward";
+            this.buttonForward.UseVisualStyleBackColor = true;
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(680, 12);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 5;
+            this.buttonGo.Text = "Accept";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(545, 332);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.buttonForward);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.webBrowserApp);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowserApp;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
